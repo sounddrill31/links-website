8a256cb1fe865e7ba8ed480c9eb93a0a7ab8cbbb
Trevor Lee <49603972+trevortylerlee@users.noreply.github.com>
2024-10-05 13:25:22 -0700

    quick save

--- src/siteConfig.ts
+++ src/siteConfig.ts
@@ -2,7 +2,7 @@ import type { SiteConfiguration, SocialLinks } from "@/types.ts";
 
 export const SITE: SiteConfiguration = {
   name: "Trevor Tyler Lee",
-  bio: "LinkTree alternative built with Astro & Tailwind CSS.",
+  bio: "Free, open source LinkTree alternative built with Astro & Tailwind CSS.",
   url: "https://astro-link.vercel.app",
   locale: "en-CA",
 };
