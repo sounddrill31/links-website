a097e03dc71a3a5e61f68bddfce7c9ff08899844
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-03-27 19:36:20 +0530

    Update and rename quick-CI.sh to quick-CI-build.sh

--- quick-CI-build.sh
+++ quick-CI-build.sh
@@ -1,19 +1,14 @@
 #/usr/bin/bash
 
-rm -rf links-website
-
-git clone https://github.com/sounddrill31/links-website
-
-cd links-website
+# Download repo
+#rm -rf links-website
+#git clone https://github.com/sounddrill31/links-website
+#cd links-website
 
 wget https://github.com/sounddrill31/links-website/releases/download/27Mar2025/site-config.zip
-
 unzip site-config.zip -d .
-
-nvm use node
-
-npm install
+#nvm use node
+#mv * ..
+#cd ..
 
 npm run build
-
-mv dist ..
